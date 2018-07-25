91a7526f3b7ee0ab66ac770188225e89376a9a61
Sam Martin <sam.martin@7digital.com>
2018-07-25 16:36:21 +0100

    Working authorisation server cookbook

--- recipes/authorisation_server.rb
+++ recipes/authorisation_server.rb
@@ -15,7 +15,7 @@ node['seven_digital_acme_helper']['certificates'].each do |certificate|
   end
 
   # Upload the certificate
-  seven_digital_acme_helper_certificate_set certificate['domain'] do
+  seven_digital_acme_helper_certificate_databag_upload certificate['domain'] do
     data_bag_name node['seven_digital_acme_helper']['data_bag_name']
     data_bag_secret node['seven_digital_acme_helper']['secret']
     key certificate['key']
