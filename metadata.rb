4d0dff21f03c7017fb28a387e22dd41947130421
Sam Martin <sam.martin@7digital.com>
2018-07-26 14:40:49 +0100

    Added necessary dependency

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures seven_digital_acme_helper'
 long_description 'Installs/Configures seven_digital_acme_helper'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends          'acme'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
